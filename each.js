f21aab63f0b4f565ff8c528c98dad14cc2f82e2a
Rowad <rowadwo@gmail.com>
2020-04-05 23:37:44 +0300

    each with context

--- each.js
+++ each.js
@@ -6,32 +6,26 @@ console.log('~~~~~~~~~~~~~~~~~~~v0~~~~~~~~~~~~~~~~~')
 
 each([1, 2, 3], console.log)
 each({ one: 1, two: 2, three: 3 }, console.log)
-
-// eslint-disable-next-line no-unused-vars
-const each1 = (obj, fun) => {
-  if (Array.isArray(obj)) {
-    for (let i = 0, length = obj.length; i < length; i++) {
-      fun(obj[i], i, obj)
-    }
-  } else {
-    const keys = Object.keys(obj)
-    for (let i = 0, length = keys.length; i < length; i++) {
-      fun(obj[keys[i]], keys[i], obj)
-    }
+const user = {
+  friends: {
+    user1: true,
+    user2: false,
+    user3: true,
+    user4: false
   }
-  return obj
 }
 
-console.log('~~~~~~~~~~~~~~~~~~~v1~~~~~~~~~~~~~~~~~')
+each(
+  ['user1', 'user4', 'user5'],
+  function (val) {
+    console.log(this[val])
+  },
+  user.friends
+)
 
-each1([1, 2, 3], console.log)
-each1({ one: 1, two: 2, three: 3 }, (val, key, obj) => {
-  console.log(val, key, obj)
-})
-
-const each2 = (obj, fun) => {
+const each2 = (obj, fun, context) => {
   for (const key in obj) {
-    fun(obj[key], key, obj)
+    context ? fun.call(context, obj[key], key, obj) : fun(obj[key], key, obj)
   }
   return obj
 }
@@ -42,3 +36,22 @@ each2([1, 2, 3], console.log)
 each2({ one: 1, two: 2, three: 3 }, (val, key, obj) => {
   console.log(val, key, obj)
 })
+
+const user2 = {
+  friends: {
+    user1: true,
+    user2: false,
+    user3: true,
+    user4: false
+  }
+}
+
+// this.user1 = ':)'
+
+each2(
+  ['user1', 'user4', 'user5'],
+  function (val) {
+    console.log(this[val])
+  },
+  user2.friends
+)
