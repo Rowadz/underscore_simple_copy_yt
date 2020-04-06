dff87498139764e2ffc64c199b1bb65794828ebf
Rowad <rowadwo@gmail.com>
2020-04-07 01:00:33 +0300

    better map

--- Collections/map.js
+++ Collections/map.js
@@ -4,7 +4,7 @@ const a = map([1, 2, 3], function (num) {
   return num * 3
 })
 const b = map({ one: 1, two: 2, three: 3 }, function (num, key, obh) {
-  return num * 3
+  return { num: num * 3, key }
 })
 const c = map(
   [
@@ -15,23 +15,56 @@ const c = map(
 )
 console.log('~~~~~~~~~~~~~~~~~~~v0~~~~~~~~~~~~~~~~~')
 
-console.log(a, b, c)
+console.log(a)
+console.log(b)
+console.log(c)
 
 /**
- * normal object will be returned as array of the mapped values
+ * maps the values and saves the object keys!
  * @param {*} obj - can be an Array<any> or normal object
  * @param {(any, any, any) => any} fun - the callback function
  * @param {*} context - the `this`
  */
 const map2 = (obj, fun, context) => {
-  const holder = Object.values(obj)
-  for (const key in holder) {
-    holder[key] = context
-      ? fun.call(context, holder[key], key, holder)
-      : fun(holder[key], key, holder)
+  const holder = []
+  for (const key in obj) {
+    holder.push(
+      context ? fun.call(context, obj[key], key, obj) : fun(obj[key], key, obj)
+    )
   }
   return holder
 }
+
+console.log('~~~~~~~~~~~~~~~~~~~v2~~~~~~~~~~~~~~~~~')
+
+const a2 = map2([1, 2, 3], function (num) {
+  return num * 3
+})
+const b2 = map2({ one: 1, two: 2, three: 3 }, function (num, key, obh) {
+  return { num, key }
+})
+const c2 = map2(
+  [
+    [1, 2],
+    [3, 4]
+  ],
+  ([f]) => f
+)
+const d2 = map2(
+  [
+    { name: 'rowad', score: 10 },
+    { name: 'sarah', score: 20 },
+    { name: 'rowadz', score: 30 }
+  ],
+  ({ score, name }, idx, arr) => ({ name, score: score ** 2 })
+)
+
+console.log(a2)
+console.log(b2)
+console.log(c2)
+console.log(d2)
+
+console.log('~~~~~~~~~~~~~~~~~~~v3~~~~~~~~~~~~~~~~~')
 
 /**
  * maps the values and saves the object keys!
@@ -48,39 +81,10 @@ const map3 = (obj, fun, context) => {
   return obj
 }
 
-console.log('~~~~~~~~~~~~~~~~~~~v2~~~~~~~~~~~~~~~~~')
-
-const a2 = map2([1, 2, 3], function (num) {
-  return num * 3
-})
-const b2 = map2({ one: 1, two: 2, three: 3 }, function (num, key, obh) {
-  return num * 3
-})
-const c2 = map2(
-  [
-    [1, 2],
-    [3, 4]
-  ],
-  ([f]) => f
-)
-
-console.log(a2, b2, c2)
-
-console.log('~~~~~~~~~~~~~~~~~~~v3~~~~~~~~~~~~~~~~~')
-
-const a3 = map3([1, 2, 3], function (num) {
-  return num * 3
-})
 const b3 = map3({ one: 1, two: 2, three: 3 }, function (num, key, obh) {
-  return num * 3
+  return num ** 2
 })
-const c3 = map3(
-  [
-    [1, 2],
-    [3, 4]
-  ],
-  ([f]) => f
-)
+
 const d3 = map3(
   [
     { name: 'rowad', score: 10 },
@@ -90,4 +94,5 @@ const d3 = map3(
   ({ score, name }, idx, arr) => ({ name, score: score ** 2 })
 )
 
-console.log(a3, b3, c3, d3)
+console.log(d3)
+console.log(b3)
