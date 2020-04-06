02012d893019e42bc8b1aea31237c1fab1250023
Rowad <rowadwo@gmail.com>
2020-04-07 02:12:42 +0300

    fix :(

--- Collections/reduce.js
+++ Collections/reduce.js
@@ -24,39 +24,3 @@ const groupedData = reduce(
 console.log('~~~~~~~~~~~~~~~~~~~v0~~~~~~~~~~~~~~~~~')
 console.log(sum) // 6
 console.log(groupedData) // { rowad: 50, sarah: 60 }
-
-/**
- *
- * @param {*} obj - can be an Array<any> or normal object
- * @param {(any, any, any, any) => any} fun - the callback function
- * @param {*} memo - the init value
- * @param {*} context - the `this`
- */
-const reduce2 = (obj, fun, memo, context) => {
-  const values = Object.values(obj)
-  if (!memo) {
-    memo = values.shift()
-  }
-  for (const key in values) {
-    memo = context
-      ? fun.call(context, memo, values[key], key, values)
-      : fun(memo, values[key], key, values)
-  }
-  return memo
-}
-
-const sum2 = reduce2([1, 2, 3], (memo, num) => memo + num)
-const sum3 = reduce2({ age1: 1, age2: 2, age3: 3 }, (memo, num) => memo + num)
-const groupedData2 = reduce2(
-  data,
-  (memo, { name, score }) => ({
-    ...memo,
-    [name]: score + (memo[name] || 0)
-  }),
-  {}
-)
-
-console.log('~~~~~~~~~~~~~~~~~~~v1~~~~~~~~~~~~~~~~~')
-console.log(sum2) // 6
-console.log(sum3) // 6
-console.log(groupedData2) // { rowad: 50, sarah: 60 }
--- Collections/reduceRight.js
+++ Collections/reduceRight.js
@@ -8,27 +8,3 @@ const list = [
 const flat = reduceRight(list, (a, b) => [...a, ...b], [])
 console.log('~~~~~~~~~~~~~~~~~~~v0~~~~~~~~~~~~~~~~~')
 console.log(flat)
-
-/**
- *
- * @param {*} obj - can be an Array<any> or normal object
- * @param {(any, any, any, any) => any} fun - the callback function
- * @param {*} memo - the init value
- * @param {*} context - the `this`
- */
-const reduceRight2 = (obj, fun, memo, context) => {
-  const values = Object.values(obj).reverse()
-  if (!memo) {
-    memo = values.shift()
-  }
-  for (const key in values) {
-    memo = context
-      ? fun.call(context, memo, values[key], key, values)
-      : fun(memo, values[key], key, values)
-  }
-  return memo
-}
-
-const flat2 = reduceRight2(list, (a, b) => [...a, ...b], [])
-console.log('~~~~~~~~~~~~~~~~~~~v01~~~~~~~~~~~~~~~~~')
-console.log(flat2)
