1ffe8b8d118731b5f0cef96d419b8c89dafa4a22
Rowad <rowadwo@gmail.com>
2020-04-07 23:42:36 +0300

    reduce --correct

--- Collections/reduce.js
+++ Collections/reduce.js
@@ -22,5 +22,49 @@ const groupedData = reduce(
 )
 
 console.log('~~~~~~~~~~~~~~~~~~~v0~~~~~~~~~~~~~~~~~')
-console.log(sum) // 6
-console.log(groupedData) // { rowad: 50, sarah: 60 }
+// console.log(sum) // 6
+// console.log(groupedData) // { rowad: 50, sarah: 60 }
+
+const reduce2 = (obj, fun, memo, context) => {
+  let arrHolder
+  let objHolder
+  const isArr = Array.isArray(obj)
+  if (!memo) {
+    if (isArr) {
+      memo = obj.shift()
+      arrHolder = [...obj]
+    } else {
+      const key = Object.keys(obj)[0]
+      memo = obj[key]
+      objHolder = { ...obj }
+      delete objHolder[key]
+    }
+  }
+  for (const key in isArr ? arrHolder || obj : objHolder || obj) {
+    memo = fun(memo, obj[key], key, obj)
+  }
+  return memo
+}
+
+const sum2 = reduce2([1, -1, 3], (memo, num) => memo + num)
+const groupedData2 = reduce2(
+  data,
+  (memo, { name, score }) => ({
+    ...memo,
+    [name]: score + (memo[name] || 0)
+  }),
+  {}
+)
+
+const test = reduce2(
+  { sum1: 1, sum2: 30, sum3: 100 },
+  (memo, val, key) => ({
+    sum: val + (memo.sum || 0)
+  }),
+  {}
+)
+
+console.log('~~~~~~~~~~~~~~~~~~~v1~~~~~~~~~~~~~~~~~')
+console.log(sum2) // 6
+console.log(groupedData2) // { rowad: 50, sarah: 60 }
+console.log(test)
