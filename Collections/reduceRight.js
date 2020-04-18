697c124f9fe00248792560827e7ac236b640ea80
Rowad <rowadwo@gmail.com>
2020-04-18 22:59:41 +0300

    reduce Right

--- Collections/reduceRight.js
+++ Collections/reduceRight.js
@@ -8,3 +8,42 @@ const list = [
 const flat = reduceRight(list, (a, b) => [...a, ...b], [])
 console.log('~~~~~~~~~~~~~~~~~~~v0~~~~~~~~~~~~~~~~~')
 console.log(flat)
+
+
+const reduceRight2 = (obj, fun, memo, context) => {
+  const isArr = Array.isArray(obj)
+  let arrHolder
+  let objHolder
+  if (!memo) {
+    if (isArr) {
+      memo = obj.shift()
+      arrHolder = [...obj]
+      obj.unshift(memo)
+    } else {
+      const [key] = Object.keys(obj)
+      memo = obj[key]
+      objHolder = { ...obj }
+      delete objHolder[key]
+    }
+  }
+  if (isArr){
+    (arrHolder || obj).reverse().forEach((_, key) => {
+      memo = context
+        ? fun.call(context, memo, (arrHolder || obj)[key], key, obj)
+        : fun(memo, (arrHolder || obj)[key], key, obj)
+    })
+  } else {
+    const toLoop = objHolder ? Object.keys(obj).reverse() : Object.keys(objHolder).reverse()
+    toLoop.forEach((_, key) => {
+      memo = context
+        ? fun.call(context, memo, toLoop[key], key, obj)
+        : fun(memo, toLoop[key], key, obj)
+    })
+  }
+  return memo
+}
+
+
+const flat2 = reduceRight2(list, (a, b) => [...a, ...b], [])
+console.log('~~~~~~~~~~~~~~~~~~~v1~~~~~~~~~~~~~~~~~')
+console.log(flat2)
