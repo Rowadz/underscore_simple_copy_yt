02be6d89029b3a27442001951d0aeab2e3e8c77a
Rowad <rowadwo@gmail.com>
2020-04-18 23:23:54 +0300

    findWhere

---
+++ Collections/findWhere.js
@@ -0,0 +1,42 @@
+const { findWhere } = require('underscore')
+
+const dataArr = [
+  { name: 'rowad', age: 22 },
+  { name: 'rowad', age: 21 },
+  { name: 'rowad', age: 26, id: 1 },
+  { name: 'rowad', age: 19 },
+  { name: 'rowad', age: 18 },
+  { name: 'rowad', age: 17 },
+  { name: 'rowad', age: 26, id: 2 }
+]
+
+const res = findWhere(dataArr, {
+  name: 'rowad',
+  age: 26
+})
+
+console.log(res)
+
+const findWhere2 = (list, properties) => {
+  const propKeys = Object.keys(properties)
+  for (const item of list) {
+    for (const _ in item) {
+      let thisItem = true
+      for (const propKey of propKeys) {
+        if (!(propKey in item) || properties[propKey] !== item[propKey]) {
+          thisItem = false
+          break
+        }
+      }
+      if (thisItem) return item
+    }
+  }
+  return undefined
+}
+
+const res2 = findWhere2(dataArr, {
+  name: 'rowad',
+  age: 26
+})
+
+console.log(res2)
