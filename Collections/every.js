25a27d4841ad4459562ae504148d9e60d0a9bd66
Rowad <rowadwo@gmail.com>
2020-04-18 23:58:02 +0300

    every

--- Collections/every.js
+++ Collections/every.js
@@ -1,16 +1,20 @@
 const { every } = require('underscore')
 
 const isAllEven = every([2, 4, 5], (num) => num % 2 == 0)
-
 console.log(isAllEven)
+console.log(every([]))
+console.log(every([null]))
 
 const every2 = (list, fun, context) => {
   let res = true
   for (const item of list)
-    if (!(context ? fun.call(context, item) : fun(item))) return false
+    if (fun ? !(context ? fun.call(context, item) : fun(item)) : !item)
+      return false
   return res
 }
 
 const isAllEven2 = every2([2, 4, 5], (num) => num % 2 == 0)
 
 console.log(isAllEven2)
+console.log(every2([]))
+console.log(every2([null]))
