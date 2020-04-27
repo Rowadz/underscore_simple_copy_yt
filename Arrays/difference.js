556a70cb98566fde68bb867adade6d679efa72a1
MohammedAl-rowad <rowadwo@gmail.com>
2020-04-27 23:07:21 +0300

    difference

--- Arrays/difference.js
+++ Arrays/difference.js
@@ -2,11 +2,11 @@ const { difference } = require('underscore');
 
 console.log(difference([1, 2, 3, 4, 5], [5, 2, 10]));
 
-function difference2(array, ...others) {
+const difference2 = (array, ...others) => {
   others.forEach((e) => {
     array = array.filter((d) => !new Set(e).has(d));
   });
   return array;
-}
+};
 
 console.log(difference2([1, 2, 3, 4, 5], [5, 2, 10]));
