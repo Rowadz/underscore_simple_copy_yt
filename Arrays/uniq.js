d7891d9d7d4bcb0bebeb6f170f955ec243d09c75
MohammedAl-rowad <rowadwo@gmail.com>
2020-04-27 23:22:43 +0300

    uniq

---
+++ Arrays/uniq.js
@@ -0,0 +1,27 @@
+const { uniq } = require('underscore');
+const data = [
+  { name: 'rowad', age: 21 },
+  { name: 'sarah', age: 21 },
+  { name: 'polo', age: 22 },
+];
+
+console.log(uniq([1, 2, 1, 4, 1, 3]));
+console.log(uniq(data, false, ({ age }) => age));
+
+const uniq2 = (array, _, fun) => {
+  const map = {};
+  const res = [];
+  for (const item of array) {
+    let val;
+    if (fun) val = fun(item);
+    else val = item;
+    if (!map[val]) {
+      map[val] = true;
+      res.push(item);
+    }
+  }
+  return res
+};
+
+console.log(uniq2([1, 2, 1, 4, 1, 3]));
+console.log(uniq2(data, false, ({ age }) => age));
