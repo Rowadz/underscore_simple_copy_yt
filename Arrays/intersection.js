0a35112134e69a7061cd95f3cd43133310ecc863
MohammedAl-rowad <rowadwo@gmail.com>
2020-04-27 19:05:26 +0300

    fix intersection

--- Arrays/intersection.js
+++ Arrays/intersection.js
@@ -3,31 +3,18 @@ const { intersection } = require('underscore');
 const data = [
   [1, 2, 3],
   [101, 2, 1, 10],
-  [2, 1],
+  [2, 99],
 ];
 
 console.log(intersection(...data));
 
-const intersection2 = (...arrays) => {
-  const minSet = new Set(
-    arrays.reduce(
-      (prev, curr) => (prev.length > curr.length ? curr : prev),
-      arrays[0]
-    )
-  );
-  const map = {};
-  const res = [];
-  for (let i = 0; i < arrays.length; i++) {
-    const arr = arrays[i];
-    for (let j = 0; j < arr.length; j++) {
-      if (minSet.has(arr[j]) && !map[arr[j]]) {
-        map[arr[j]] = true;
-        res.push(arr[j]);
-      }
-    }
-  }
+function intersection2(...arrays) {
+  let res = arrays[0]
+  arrays = arrays.slice(1)
+  arrays.forEach((e) => {
+    res = res.filter(Set.prototype.has, new Set(e));
+  })
   return res
-};
-
+}
 
 console.log(intersection2(...data));
